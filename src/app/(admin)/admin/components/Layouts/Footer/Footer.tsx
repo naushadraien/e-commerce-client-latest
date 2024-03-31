2351f50a4fbb962b436d4043e819f0796d76d8ae
naushadraien <naushadraien5@gmail.com>
2024-03-31 19:43:31 +0545

    feat: footer

--- src/app/(admin)/admin/components/Layouts/Footer/Footer.tsx
+++ src/app/(admin)/admin/components/Layouts/Footer/Footer.tsx
@@ -1,5 +1,21 @@
+import Link from "next/link";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="w-full border-t-[1px] border-t-gray-400 text-center h-10">
+      <div>
+        Copyright © 2024 Rehan. Design with 💘 by{" "}
+        <Link
+          href="https://naushadraien.com.np"
+          target="_blank"
+          className="text-purple-500 hover:text-purple-700 font-semibold"
+        >
+          Naushad
+        </Link>{" "}
+        All rights reserved.
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
